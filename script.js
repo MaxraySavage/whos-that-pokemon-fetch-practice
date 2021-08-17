bb7c282611d429aaa1821162a64e16a941de2b0a
MaxraySavage <MaxraySavage@users.noreply.github.com>
2021-08-17 11:54:44 -0400

    trying diffrent drawing method

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 testURL = 'https://pokeapi.co/api/v2/pokemon/ditto';
 
-const canvasHandler = {
-    canvas: document.getElementById('main-canvas'),
-    ctx: this.canvas.getContext("2d"),
-    drawImage: function(imageElement) {
-        this.ctx.drawImage(imageElement, 10, 10);
-    },
-};
+
+// from https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/drawImage
+function drawToCanvas() {
+    // Use the intrinsic size of image in CSS pixels for the canvas element
+    canvas.width = this.naturalWidth;
+    canvas.height = this.naturalHeight;
+    
+    // Will draw the image as 300x227, ignoring the custom size of 60x45
+    // given in the constructor
+    ctx.drawImage(this, 0, 0);
+    
+    // To use the custom size we'll have to specify the scale parameters
+    // using the element's width and height properties - lets draw one
+    // on top in the corner:
+    //ctx.drawImage(this, 0, 0, this.width, this.height);
+}
 
 
 fetch(testURL).then((response) => response.json()).then((jsonData) =>{
     pokemonSprite = jsonData.sprites.front_default;
     console.log(pokemonSprite);
     const pokeImage = new Image();
+    pokeImage.onload = drawToCanvas;
     pokeImage.src = pokemonSprite;
-    canvasHandler.drawImage(pokeImage);
+
 });
