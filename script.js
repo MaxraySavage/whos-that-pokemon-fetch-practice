587eebfeb695b57e811a3231fa0465cec60a43f1
MaxraySavage <MaxraySavage@users.noreply.github.com>
2021-08-17 11:31:48 -0400

    test pokemon sprite fetch

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 testURL = 'https://pokeapi.co/api/v2/pokemon/ditto';
 
 fetch(testURL).then((response) => response.json()).then((jsonData) =>{
-    console.log(jsonData);
+    pokemonSprite = jsonData.sprites.front_default;
+    console.log(pokemonSprite);
 });
