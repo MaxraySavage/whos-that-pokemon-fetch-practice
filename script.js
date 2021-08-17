f0a80d34b9d30ded33e1964a8b77bb8c64cf5a22
MaxraySavage <MaxraySavage@users.noreply.github.com>
2021-08-17 11:47:50 -0400

    Test drawing to canvas

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 testURL = 'https://pokeapi.co/api/v2/pokemon/ditto';
 
+const canvasHandler = {
+    canvas: document.getElementById('main-canvas'),
+    ctx: this.canvas.getContext("2d");
+    drawImage: function(imageElement) {
+        this.ctx.drawImage(imageElement, 10, 10);
+    }
+};
+
+
 fetch(testURL).then((response) => response.json()).then((jsonData) =>{
     pokemonSprite = jsonData.sprites.front_default;
     console.log(pokemonSprite);
+    const pokeImage = new Image();
+    pokeImage.src = pokemonSprite;
+    canvasHandler.drawImage(pokeImage);
 });
